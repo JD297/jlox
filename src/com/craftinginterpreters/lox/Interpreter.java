0555e1bdd688512280fe5208e9a807c6cb9d2544
JD297 <jan.dommasch297@gmail.com>
2025-11-09 20:08:24 +0100

    Implemented chapter 9: Control Flow

--- src/com/craftinginterpreters/lox/Interpreter.java
+++ src/com/craftinginterpreters/lox/Interpreter.java
@@ -12,6 +12,23 @@ class Interpreter implements Expr.Visitor<Object>,
 	}
 
 	@Override
+	public Object visitLogicalExpr(Expr.Logical expr) {
+		Object left = evaluate(expr.left);
+
+		if (expr.operator.type == TokenType.OR) {
+			if (isTruthy(left)) {
+				return left;
+			}
+		} else {
+			if (!isTruthy(left)) {
+				return left;
+			}
+		}
+
+		return evaluate(expr.right);
+	}
+
+	@Override
 	public Object visitUnaryExpr(Expr.Unary expr) {
 		Object right = evaluate(expr.right);
 
@@ -144,6 +161,17 @@ class Interpreter implements Expr.Visitor<Object>,
 	}
 
 	@Override
+	public Void visitIfStmt(Stmt.If stmt) {
+		if (isTruthy(evaluate(stmt.condition))) {
+			execute(stmt.thenBranch);
+		} else if (stmt.elseBranch != null) {
+			execute(stmt.elseBranch);
+		}
+
+		return null;
+	}
+
+	@Override
 	public Void visitPrintStmt(Stmt.Print stmt) {
 		Object value = evaluate(stmt.expression);
 
@@ -161,6 +189,15 @@ class Interpreter implements Expr.Visitor<Object>,
 		}
 
 		environment.define(stmt.name.lexeme, value);
+
+		return null;
+	}
+
+	@Override
+	public Void visitWhileStmt(Stmt.While stmt) {
+		while (isTruthy(evaluate(stmt.condition))) {
+			execute(stmt.body);
+		}
 
 		return null;
 	}
